0e9087f228b65a7cc91860de24f71a714990d1fb
ollie <olivinanzali07@gmail.com>
2023-03-12 15:55:01 +0200

    updated schema

--- recipes-schema.sql
+++ recipes-schema.sql
@@ -3,13 +3,12 @@ CREATE DATABASE recipes_db;
 DROP DATABASE recipes_db;
 \connect recipes_db
 
-CREATE TABLE user (
-    username VARCHAR(25) PRIMARY KEY,
-    password TEXT NOT NULL,
-    first_name TEXT NOT NULL,
-    last_name TEXT NOT NULL,
-    email TEXT NOT NULL
-    CHECK (position('@' IN email) > 1)
+CREATE TABLE users ( 
+    username VARCHAR(25) PRIMARY KEY, 
+    password TEXT NOT NULL, 
+    first_name TEXT NOT NULL, 
+    last_name TEXT NOT NULL, 
+    email TEXT NOT NULL CHECK (position('@' IN email) > 1)
 );
 
 CREATE TABLE recipes (
@@ -19,18 +18,13 @@ CREATE TABLE recipes (
     instruction TEXT NOT NULL,
     category TEXT NOT NULL,
     image_url TEXT NOT NULL
-    REFERENCES user ON DELETE CASCADE
+    REFERENCES users ON DELETE CASCADE
 );
 
-CREATE TABLE ingredients (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    description TEXT NOT NULL,
-    recipes_id INTEGER REFERENCES recipes ON DELETE 
-);
+
 
 CREATE TABLE categories (
     id SERIAL PRIMARY KEY,
-    category TEXT NOT NULL
+    category TEXT NOT NULL,
     recipes_id INTEGER REFERENCES recipes ON DELETE CASCADE
-)
+);
